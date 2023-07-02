dcc0e1f250b56ecac78dd3b6f374e9d00ec43b83
Nate <nate12309@LAPTOP-CNADA5S6.localdomain>
2023-07-01 20:17:01 -0400

    creates myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+function appendCat(name) {
+  const newCats = [...cats, name];
+  return newCats;
+}
+function prependCat(name) {
+  const newCats = [name, ...cats];
+  return newCats;
+}
+function removeFirstCat() {
+  const newCats = cats.slice(1);
+  return newCats;
+}
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+function removeLastCat() {
+  const newCats = cats.slice(0, -1);
+  return newCats;
+}
